9e266b5ae84a670ffd238bf73a976aadb8fc833c
Seth Carney <seth.carney@lanetix.com>
2014-03-10 12:26:51 -0400

    add underscore for options processing. add nosanitization option

--- test/parse.js
+++ test/parse.js
@@ -1,53 +1,61 @@
-/*globals it, describe */
-var mocha  = require('mocha'),
-    should = require('should'),
-    hstore = require('../lib/index.js');
+/*globals it, describe, beforeEach */
+(function () {
+  'use strict';
 
-describe('pg-hstore.parse', function() {
-  it('should hstore parse an hstore string', function(done) {
-    var source = '"foo"=>"bar"';
-    hstore.parse(source, function(target) {
-      should.exist(target);
-      target.foo.should.equal('bar');
-      done();
+  var should = require('should'),
+      HStore = require('../lib/index.js'),
+      hstore;
+
+  describe('pg-hstore.parse', function () {
+    beforeEach(function () {
+      hstore = new HStore();
+    });
+
+    it('should hstore parse an hstore string', function (done) {
+      var source = '"foo"=>"bar"';
+      hstore.parse(source, function (target) {
+        should.exist(target);
+        target.foo.should.equal('bar');
+        done();
+      });
+    });
+
+    it('should hstore parse an hstore string with multiple values', function (done) {
+      var source = '"foo"=>"oof","bar"=>"rab","baz"=>"zab"';
+      hstore.parse(source, function (target) {
+        should.exist(target);
+        target.foo.should.equal('oof');
+        target.bar.should.equal('rab');
+        target.baz.should.equal('zab');
+        done();
+      });
+    });
+
+    it('should hstore parse an escaped quoted string with quotes', function (done) {
+      var source = '"foo"=>"\\"bar\\""';
+      hstore.parse(source, function (target) {
+        should.exist(target);
+        target.foo.should.equal('"bar"');
+        done();
+      });
+    });
+
+    it('should hstore parse a string with commas', function (done) {
+      var source = '"foo"=>"bar,foo,bar"';
+      hstore.parse(source, function (target) {
+        should.exist(target);
+        target.foo.should.equal('bar,foo,bar');
+        done();
+      });
+    });
+
+    it('should hstore parse a string with advanced types', function (done) {
+      var source = '"foo"=>"{\\"key\\":\\"value\\",\\"key2\\":\\"value\\"}"';
+      hstore.parse(source, function (target) {
+        should.exist(target);
+        target.foo.should.equal('{"key":"value","key2":"value"}');
+        done();
+      });
     });
   });
-
-  it('should hstore parse an hstore string with multiple values', function(done) {
-    var source = '"foo"=>"oof","bar"=>"rab","baz"=>"zab"';
-    hstore.parse(source, function(target) {
-      should.exist(target);
-      target.foo.should.equal('oof');
-      target.bar.should.equal('rab');
-      target.baz.should.equal('zab');
-      done();
-    });
-  });
-
-  it('should hstore parse an escaped quoted string with quotes', function(done) {
-    var source = '"foo"=>"\\"bar\\""';
-    hstore.parse(source, function(target) {
-      should.exist(target);
-      target.foo.should.equal('"bar"');
-      done();
-    });
-  });
-
-  it('should hstore parse a string with commas', function(done) {
-    var source = '"foo"=>"bar,foo,bar"';
-    hstore.parse(source, function(target) {
-      should.exist(target);
-      target.foo.should.equal('bar,foo,bar');
-      done();
-    });
-  });
-
-  it('should hstore parse a string with advanced types', function(done) {
-    var source = '"foo"=>"{\\"key\\":\\"value\\",\\"key2\\":\\"value\\"}"';
-    hstore.parse(source, function(target) {
-      should.exist(target);
-      target.foo.should.equal('{"key":"value","key2":"value"}');
-      done();
-    });
-  });
-});
+})();
--- test/stringify.js
+++ test/stringify.js
@@ -1,87 +1,104 @@
-/*globals it, describe */
-var mocha  = require('mocha'),
-    should = require('should'),
-    hstore = require('../lib/index.js');
+/*globals it, describe, beforeEach */
+(function () {
+  'use strict';
 
-describe('pg-hstore.stringify', function() {
-  it('should hstore encode a string', function(done) {
-    var source = { foo: "bar" };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>"bar"');
-      done();
+  var should = require('should'),
+      HStore = require('../lib/index.js'),
+      hstore;
+
+  describe('pg-hstore.stringify', function () {
+    beforeEach(function () {
+      hstore = new HStore();
+    });
+
+    it('should hstore encode a string', function (done) {
+      var source = { foo: 'bar' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>"bar"');
+        done();
+      });
+    });
+
+    it('should hstore encode a number', function (done) {
+      var source = { foo: 1000 };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>"1000"');
+        done();
+      });
+    });
+
+    it('should hstore encode a boolean', function (done) {
+      var source = { foo: true };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>"true"');
+        done();
+      });
+    });
+
+    it('should hstore encode a null value', function (done) {
+      var source = { foo: null };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>NULL');
+        done();
+      });
+    });
+
+    it('should hstore encode a null string value', function (done) {
+      var source = { foo: 'null' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>"null"');
+        done();
+      });
+    });
+
+    it('should hstore encode single quotes correctly', function (done) {
+      var source = { 'foo \'quotes\'': 'with \'quotes\'' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo \'quotes\'"=>"with \'quotes\'"');
+        done();
+      });
+    });
+
+    it('should hstore encode double quotes correctly', function (done) {
+      var source = { foo: 'with \"quotes\"' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>"with "quotes""');
+        done();
+      });
+    });
+
+    it('should hstore encode double quote keys correctly', function (done) {
+      var source = { 'foo \"quotes\"': 'with \"quotes\"' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo "quotes""=>"with "quotes""');
+        done();
+      });
+    });
+
+    it('should hstore encode colon correctly', function (done) {
+      var source = { 'foo': 'with:colon' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo"=>"with:colon"');
+        done();
+      });
+    });
+
+    it('should not sanitize output', function (done) {
+      var source = { 'foo\'"\\': 'bar' };
+      hstore.stringify(source, function (target) {
+        should.exist(target);
+        target.should.equal('"foo\'"\\"=>"bar"');
+        done();
+      }, true);
     });
   });
-
-  it('should hstore encode a number', function(done) {
-    var source = { foo: 1000 };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>"1000"');
-      done();
-    });
-  });
-
-  it('should hstore encode a boolean', function(done) {
-    var source = { foo: true };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>"true"');
-      done();
-    });
-  });
-
-  it('should hstore encode a null value', function(done) {
-    var source = { foo: null };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>NULL');
-      done();
-    });
-  });
-
-  it('should hstore encode a null string value', function(done) {
-    var source = { foo: "null" };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>"null"');
-      done();
-    });
-  });
-
-  it('should hstore encode single quotes correctly', function(done) {
-    var source = { 'foo \'quotes\'': "with \'quotes\'" };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo \'\'quotes\'\'"=>"with \'\'quotes\'\'"');
-      done();
-    });
-  });
-
-  it('should hstore encode double quotes correctly', function(done) {
-    var source = { foo: "with \"quotes\"" };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>"with \\"quotes\\""');
-      done();
-    });
-  });
-
-  it('should hstore encode double quote keys correctly', function(done) {
-    var source = { 'foo \"quotes\"': "with \"quotes\"" };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo \\"quotes\\""=>"with \\"quotes\\""');
-      done();
-    });
-  });
-
-  it('should hstore encode colon correctly', function(done) {
-    var source = { 'foo': "with:colon" };
-    hstore.stringify(source, function(target) {
-      should.exist(target);
-      target.should.equal('"foo"=>"with\\:colon"');
-      done();
-    });
-  });
-});
+})();
